522c56e24521e8a81226a3013898b039cc6d3d3b
Gabriel-Azevedo-Batalha <batalha800@gmail.com>
2019-09-04 10:01:23 -0300

    Adicionado os gets e sets restantes

--- aula5/src/caminhao.cpp
+++ aula5/src/caminhao.cpp
@@ -9,17 +9,53 @@ Caminhao::Caminhao(string marca)
     setVelocidadeMaxima(180.0);
     cout << "Construtor do caminhao" << endl;
     cout << "Caminhão " << marca;
-
+    setMarca(marca);
 }
-
-
 Caminhao::Caminhao(float _velocidadeMaxima, string marca)//:Carro(_velocidadeMaxima)
 {
     carga = 0.0;
     comprimento = 0.0;
     altura = 0.0;
- //   setVelocidadeMaxima(_velocidadeMaxima);
+    setVelocidadeMaxima(_velocidadeMaxima);
     cout << "Construtor do caminhao" << endl;
     cout << "Caminhão: " << marca << endl;
-
+    setMarca(marca);
+}
+void Caminhao::setTipoCarga(string tcarga)
+{
+    tipoCarga = tcarga;
+}
+string Caminhao::getTipoCarga()
+{
+    return this->tipoCarga;
+}
+void Caminhao::setComprimento(float _comprimento)
+{
+    if (_comprimento > 0.0){
+        comprimento = _comprimento;
+    }
+}
+float Caminhao::getComprimento()
+{
+    return this->comprimento;
+}
+void Caminhao::setCarga(float _carga)
+{
+    if (_carga >= 0.0){
+        carga = _carga;
+    }
+}
+float Caminhao::getCarga()
+{
+    return this->carga;
+}
+void Caminhao::setAltura(float _altura)
+{
+    if (_altura > 0.0){
+        altura = _altura;
+    }
+}
+float Caminhao::getAltura()
+{
+    return this->altura;
 }
--- aula5/src/carro.cpp
+++ aula5/src/carro.cpp
@@ -9,9 +9,63 @@ Carro::~Carro()
 {
     cout << "Destriu o carro" << endl;
 }
+string Carro::getMarca()
+{
+    return this->marca;
+}
+void Carro::setMarca(string _marca)
+{
+    marca = _marca;
+}
+int Carro::getAno()
+{
+    return this->ano;
+}
+void Carro::setAno(int _ano)
+{
+    if(_ano >= 1886){
+        ano = _ano;
+        }
+}
+string Carro::getModelo()
+{
+    return this->modelo;
+}
+void Carro::setModelo(string _modelo)
+{
+    modelo = _modelo;
+}
+int Carro::getCapacidade()
+{
+    return this->capacidade;
+}
+void Carro::setCapacidade(int qtePessoas)
+{
+    if (qtePessoas > 0){
+        capacidade = qtePessoas;
+    }
+}
+string Carro::getCor()
+{
+    return this->cor;
+}
+void Carro::setCor(string _cor)
+{
+    cor = _cor;
+}
+float Carro::getPreco()
+{
+    return this->preco;
+}
+void Carro::setPreco(float _preco)
+{
+    if(_preco > 0){
+        preco = _preco;
+    }
+}
 void Carro::setVelocidadeAtual(float velocidade)
 {
-    if(velocidade >= 0.0){
+    if(velocidade >= 0.0 && velocidade <= velocidadeMax){
         velocidadeAtual = velocidade;
     }
 }
@@ -22,7 +76,7 @@ float Carro::getVelocidadeAtual()
 void Carro::setVelocidadeMaxima(float velocidade)
 {
     cout << "setou velocidade maxima: "<< velocidade << endl;
-    if(0.0 <= velocidade < 400.00){
+    if(0.0 <= velocidade && velocidade < 400.00){
         this->velocidadeMax =  velocidade;
     }
 }
@@ -30,13 +84,5 @@ float Carro::getVelocidadeMax()
 {
     return this->velocidadeMax;
 }
-void Carro::setCapacidade(int qtePessoas)
-{
-    if (qtePessoas > 0){
-        capacidade = qtePessoas;
-    }
-}
-int Carro::getCapacidade()
-{
-    return this->capacidade;
-}
+
+
